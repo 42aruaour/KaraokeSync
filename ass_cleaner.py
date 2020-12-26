9e795e4bd70db1c7774946fb6824f56f95bc8a84
42aruaour <42aruaour@gmail.com>
2020-12-26 02:50:50 -0500

    Added an additional conditional to only consider lines with effects if any exist; otherwise, consider default lines. Added try-catch to the processing loop to discard ass's that failed to process properly. Added another list to save all discarded filenames. Added parity check with tolerance, discarded any files with negative timings, and processed out "empty syllables." Modified the regex search and the conversion to number in lines 34 and 48, also making a huge mess on line 48 (it basically just tries to process out the number)
    Considering:
     - What to do with spaces?
     - Redoing regex in expand_times to account for all tags properly, but that would probably require a decent rewrite or expand_times

--- ass_cleaner.py
+++ ass_cleaner.py
@@ -9,6 +9,8 @@ save_dir = 'data'
 dialogue_keyword = '[Events]\n'
 effects = ['karaoke']
 
+parity_tolerance = 20
+# because apparently files aren't timed perfectly
 
 def filter_event_line(line):
     '''
@@ -23,20 +25,45 @@ def filter_event_line(line):
 def expand_times(line):
     '''
     breaks down each line by word and timing, as well as copies over start and end times per each syllable.
-    also converts start and end time to centi seconds.
+    also converts start and end time to centi seconds. If it starts with a delay, the delay is added to the start time.
     :param line:a tuple with (start time, end time, string dialogue from .ass file)
     :return: list of tuples containing (start time, end time, delay amount, word)
     '''
     dialogue_string = line[2]
-    splits = re.split("(\{\\\\k[f]?[0-9]+\})", dialogue_string)
+    splits = re.split("(\{\\\\[fF]?[kK][fF]?-?[0-9]+.*?\})", dialogue_string)
     splits = splits[1:]
     splits = list(zip(*[iter(splits)] * 2))
 
-    # also convert start and end times to centi-seconds here since it's convenient
-    start_time = (datetime.strptime(line[0], '%H:%M:%S.%f') - datetime(1900, 1, 1)).total_seconds() * 100
-    end_time = (datetime.strptime(line[1], '%H:%M:%S.%f') - datetime(1900, 1, 1)).total_seconds() * 100
+    # if the first split is a blank delay, remove and add to start time later
+    delay = 0
+    while len(re.sub("(\{\\\\.*\})", '', splits[0][1])) == 0:
+        delay = delay + float(re.search('[0-9]+', splits[0][0]).group(0))
+        splits = splits[1:]
 
-    out = map(lambda x: [start_time, end_time, float(re.search('[0-9]+', x[0]).group(0)), x[1]], splits)
+    # also convert start and end times to centi-seconds here since it's convenient
+    start_time = round((datetime.strptime(line[0], '%H:%M:%S.%f') - datetime(1900, 1, 1)).total_seconds() * 100 + delay)
+    end_time = round((datetime.strptime(line[1], '%H:%M:%S.%f') - datetime(1900, 1, 1)).total_seconds() * 100)
+
+    out = list(map(lambda x: [start_time, end_time, round(float(re.search('-?[0-9]+', str(list(filter(lambda s: 'k' in s.lower(), x[0].split('\\'))))).group(0))), re.sub("(\{\\\\.*\})", '', x[1])], splits))
+
+    # for each blank space, update the previous timing, so we don't have "blank syllables"
+    for i in range(1,len(out)):
+        j = 0
+        extension = 0
+        while len(out[i-j][3]) == 0:
+            extension = extension + out[i-j][2]
+            j = j + 1
+        out[i-j][2] = out[i-j][2] + extension
+    out = list(filter(lambda x: len(x[3]) > 0, out))
+
+    # parity check
+    # also remove any negatives
+    duration = end_time - start_time
+    parity = reduce(lambda accumulator, elem: accumulator + elem[2], out, 0)
+    negatives = reduce(lambda accumulator, elem: accumulator + (elem[2] if elem[2] < 0 else 0), out, 0)
+    if abs(parity - duration) > parity_tolerance or negatives < 0:
+        raise Exception("Duration of karaoke timings does not match duration of line, or there is a negative timing")
+
     return list(out)
 
 
@@ -59,6 +86,7 @@ expanded_data_y = []
 expanded_data_x = []
 expanded_data_lines_y = []
 expanded_data_lines_x = []
+discarded_filenames = []
 
 # look at all ass files.
 filenames = os.listdir(data_dir)
@@ -75,34 +103,49 @@ for filename in filenames:
         key = lines[0]
         lines = lines[1:]
 
-    # filter out empty lines and separate into list of (start time, end time, string to show)
-    filtered_lines = filter(lambda txt: txt.strip() != '', lines)
-    filtered_lines = filter(lambda txt: txt.split(',')[8] == '' or txt.split(',')[8] in effects, filtered_lines)
-    filtered_lines = list(map(filter_event_line, filtered_lines))
+    try:
+        # filter out empty lines and separate into list of (start time, end time, string to show)
+        filtered_lines = list(filter(lambda txt: txt.strip() != '', lines))
+        # first filter by valid effect. if the size is zero, then filter by null effect.
+        filtered_lines_effects = list(filter(lambda txt: txt.split(',')[8] in effects, filtered_lines))
+        if len(filtered_lines_effects) == 0:
+            filtered_lines = filter(lambda txt: txt.split(',')[8] == '', filtered_lines)
+        else:
+            filtered_lines = filtered_lines_effects
+        filtered_lines = list(map(filter_event_line, filtered_lines))
 
-    # separate each syllable with their time, grouped by original lyric line.
-    expanded_lines = list(map(expand_times, filtered_lines))
+        # separate each syllable with their time, grouped by original lyric line.
+        expanded_lines = list(map(expand_times, filtered_lines))
 
-    # flatmap to combine each syllable with time into one list
-    syllable_list = reduce(list.__add__, expanded_lines)
+        # filter out blank lines
+        expanded_lines = list(filter(lambda lines: len(lines) > 0, expanded_lines))
 
-    # combine each syllable per line to get list of lines, with associated list of start time for each syllable.
-    line_list = list(map(combine_syllables, expanded_lines))
+        # flatmap to combine each syllable with time into one list
+        syllable_list = reduce(list.__add__, expanded_lines)
 
-    # save in list for other processing
-    expanded_data_y.append(syllable_list)
-    expanded_data_lines_y.append(line_list)
+        # combine each syllable per line to get list of lines, with associated list of start time for each syllable.
+        line_list = list(map(combine_syllables, expanded_lines))
 
-    # get just lyrics out of list
-    expanded_data_x.append(list(map(lambda x:x[3], syllable_list)))
-    expanded_data_lines_x.append(list(map(lambda x:''.join(x[3]), line_list)))
+        # save in list for other processing
+        expanded_data_y.append(syllable_list)
+        expanded_data_lines_y.append(line_list)
 
-    # print(expanded_lines)
-    # print(syllable_list)
-    # print(line_list)
-    i = i + 1
+        # get just lyrics out of list
+        expanded_data_x.append(list(map(lambda x:x[3], syllable_list)))
+        expanded_data_lines_x.append(list(map(lambda x:''.join(x[3]), line_list)))
 
-# convert words into numbers, then convert into numpy array
+        # print(expanded_lines)
+        # print(syllable_list)
+        # print(line_list)
+        i = i + 1
+    except:
+        # If we hit an error from a malformed file, just discard it.
+        print("Discarded", filename)
+        discarded_filenames.append(filename)
+        i = i + 1
+
+print("Discarded", len(discarded_filenames), "files")
+
 
 # save as pickle
 with open(save_dir + "/syllable_x.pkl", 'wb') as file:
